07e057dca072bd52a2952e5e34f178d9e3caece4
Rodrigo Filomeno <Rodrigo Filomeno>
2016-12-02 17:06:36 -0200

    colocando abas, e adicionando variaveis no home_controller

--- src/js/controllers/home_controller.js
+++ src/js/controllers/home_controller.js
@@ -1,6 +1,6 @@
 angular.module('Pizzaria.controllers.Home', [])
 
-.controller('HomeController', function($scope) {
+.controller('HomeController', function ($scope) {
     var self = this;
     self.pizzaList = [{
             Nome: "Calabresa",
@@ -13,4 +13,42 @@ angular.module('Pizzaria.controllers.Home', [])
             Img: "img/margherita.png"
         }
     ];
+    self.bebidaList = [{
+            Nome: "Coca-cola",
+            descricao: "Refrigerante 2 litros",
+            Img: "img/coca-cola.png"
+        },
+        {
+            Nome: "Coca-Zero",
+            descricao: "Refrigerante 2 litros",
+            Img: "img/guarana.png"
+        },
+        {
+            Nome: "Guaraná",
+            descricao: "Refrigerante 2 litros",
+            Img: "img/guarana.png"
+        },
+        {
+            Nome: "Guaraná Zero",
+            descricao: "Refrigerante 2 litros",
+            Img: "img/guarana.png"
+        },
+        {
+            Nome: "Sprite",
+            descricao: "Refrigerante 2 litros",
+            Img: "img/sprite.png"
+        }
+    ];
+    self.sobremesaList = [{
+            Nome: "DIPP'S DE CANELA",
+            descricao: "A deliciosa massa da Domino's coberta de açúcar e canela, acompanhada de doce de leite.",
+            Img: "img/dipps.png"
+        },
+        {
+            Nome: "CHOCOBREAD",
+            descricao: "Massa finíssima recheada de muito chocolate e cobertura de creme de baunilha com granulado.",
+            Img: "img/chocobread.png"
+        }
+    ];
+
 });
