58d40096863b73d73365866f02cf9921009075c7
Munir Wanis <fallen.angel.pp@gmail.com>
2016-12-08 12:46:31 -0200

    do varios nada

--- www/js/dbFactory.js
+++ www/js/dbFactory.js
@@ -1,33 +1,47 @@
 angular.module("PizzariaApp")
-    .factory("DbFactory", function () {
-        var pizzas = [];
-        var db = window.openDatabase(
-            'pizzaria', // short name
-            '1.0', // version
-            'PizzariaDb', // display name
-            2 * 1024 * 1024 // size in bytes
-        );
-        db.transaction(function (tx) {
-            console.log("hue");
-            // tx.executeSql('DROP TABLE IF EXISTS pizzas'); //  INTEGER PRIMARY KEY AUTOINCREMENT
-            tx.executeSql('CREATE TABLE IF NOT EXISTS pizzas (id unique, nome, ingredientes, img, preco)');
-            tx.executeSql('INSERT INTO pizzas (id, nome, ingredientes, img, preco) SELECT 1, "Calabresa", "Mussarela, cebola, molho e orégano", "img/calabresa.png", 20.00 WHERE NOT EXISTS (SELECT 1 FROM pizzas WHERE id = 1 AND nome = "Calabresa")');
-            tx.executeSql('INSERT INTO pizzas (id, nome, ingredientes, img, preco) SELECT 2, "Margherita", "Mussarela, molho, orégano, tomate e manjericão", "img/margherita.png", 20.00 WHERE NOT EXISTS (SELECT 1 FROM pizzas WHERE id = 2 AND nome = "Margherita")');
-            tx.executeSql('SELECT nome, ingredientes, img, preco FROM pizzas', [], function (tx, results) {
-                var len = results.rows.length,
-                    i;
-                for (i = 0; i < len; i++) {
-                    console.log(results.rows.item(i));
-                    pizzas.push(results.rows.item(i));
-                }
-                console.log(pizzas);
+    .factory("DbFactory", ['$timeout', function ($timeout) {
+        var response = {};
+        // var db = window.openDatabase(
+        //     'pizzaria', // short name
+        //     '1.0', // version
+        //     'PizzariaDb', // display name
+        //     2 * 1024 * 1024 // size in bytes
+        // );
+        // db.transaction(function (tx) {
+        //     tx.executeSql('CREATE TABLE IF NOT EXISTS pizzas (id unique, nome, ingredientes, img, preco)');
+        //     tx.executeSql('INSERT INTO pizzas (id, nome, ingredientes, img, preco) SELECT 1, "Calabresa", "Mussarela, cebola, molho e orégano", "img/calabresa.png", 20.00 WHERE NOT EXISTS (SELECT 1 FROM pizzas WHERE id = 1 AND nome = "Calabresa")');
+        //     tx.executeSql('INSERT INTO pizzas (id, nome, ingredientes, img, preco) SELECT 2, "Margherita", "Mussarela, molho, orégano, tomate e manjericão", "img/margherita.png", 20.00 WHERE NOT EXISTS (SELECT 1 FROM pizzas WHERE id = 2 AND nome = "Margherita")');
+        // });
+        
+        var promise = $timeout(function () {
+            console.log('loaded');
+            response.pizzas = [];
+            var db = window.openDatabase(
+                'pizzaria', // short name
+                '1.0', // version
+                'PizzariaDb', // display name
+                2 * 1024 * 1024 // size in bytes
+            );
+            db.transaction(function (tx) {
+                tx.executeSql('CREATE TABLE IF NOT EXISTS pizzas (id unique, nome, ingredientes, img, preco)');
+                tx.executeSql('INSERT INTO pizzas (id, nome, ingredientes, img, preco) SELECT 1, "Calabresa", "Mussarela, cebola, molho e orégano", "img/calabresa.png", 20.00 WHERE NOT EXISTS (SELECT 1 FROM pizzas WHERE id = 1 AND nome = "Calabresa")');
+                tx.executeSql('INSERT INTO pizzas (id, nome, ingredientes, img, preco) SELECT 2, "Margherita", "Mussarela, molho, orégano, tomate e manjericão", "img/margherita.png", 20.00 WHERE NOT EXISTS (SELECT 1 FROM pizzas WHERE id = 2 AND nome = "Margherita")');
+                tx.executeSql('SELECT nome, ingredientes, img, preco FROM pizzas', [], function (tx, results) {
+                    console.log('transaction');
+                    var len = results.rows.length,
+                        i;
+                    for (i = 0; i < len; i++) {
+                        console.log(results.rows.item(i));
+                        response.pizzas.push(results.rows.item(i));
+                    }
+                    console.log('pizzas na promise');
+                    console.log(response.pizzas);
+                });
             });
-        });
-
-        return {
-            // pizzas: function (callback) {
-            //     callback(pizzas);
-            // }
-            pizzas: pizzas
+        }, 5000);
+        alert('test');
+        response.load = function () {
+            return promise;
         };
-    });
+        return response;
+    }]);
--- www/js/homeController.js
+++ www/js/homeController.js
@@ -1,42 +1,28 @@
 angular.module('PizzariaApp')
-        .controller('HomeController', function ($scope, DbFactory) {
+        .controller('HomeController', ['$scope', '$timeout', 'DbFactory', function ($scope, $timeout, DbFactory) {
                 var self = this;
                 $scope.filtro = '';
                 self.loadPizzasOnStart = function () {
                         $('.nav-tabs a:first').tab('show');
                 };
-                self.pizzaList = [{
-                        nome: "Calabresa",
-                        ingredientes: "Mussarela, cebola, molho e orégano",
-                        img: "img/calabresa.png"
-                }, {
-                        nome: "Marguerita",
-                        ingredientes: "Mussarela, molho, orégano, tomate e manjericão",
-                        img: "img/margherita.png"
-                }];
+                // self.pizzaList = [{
+                //         nome: "Calabresa",
+                //         ingredientes: "Mussarela, cebola, molho e orégano",
+                //         img: "img/calabresa.png"
+                // }, {
+                //         nome: "Marguerita",
+                //         ingredientes: "Mussarela, molho, orégano, tomate e manjericão",
+                //         img: "img/margherita.png"
+                // }];
                 // self.pizzaList = [];
-                // self.pizzaList = DbFactory.pizzas;
-                self.caguei = function () {
-                        console.log("caguei");
-                        console.log($scope.pizzas);
-                        console.log(self.pizzaList);
+                $scope.loaded = false;
+                DbFactory.load().then(function () {
+                        $scope.loaded = true;
+                        console.log("dbfacotry.pizzas");
                         console.log(DbFactory.pizzas);
-                };
-                // var loadPizzas = function () {
-                //         DbFactory.pizzas(function (pizzas) {
-                //                 angular.forEach(pizzas, function (pizza) {
-                //                         console.log("pizza foreach");
-                //                         console.log(pizza);
-                //                         self.pizzaList.push(pizza);
-                //                 });
-                //         });
-                //         console.log(self.pizzaList);
-                // }
-                self.loadCardapio = function () {
-                        // loadPizzas();
-                        console.log("loadPizzas");
-                        console.log(self.pizzaList);
-                }
+                });
+                self.pizzaList = DbFactory.pizzas;
+
                 self.bebidaList = [{
                         Nome: "Coca-cola",
                         Descricao: "Refrigerante 2 litros",
@@ -67,7 +53,7 @@ angular.module('PizzariaApp')
                         Descricao: "Massa finíssima recheada de muito chocolate e cobertura de creme de baunilha com granulado.",
                         Img: "img/chocobread.png"
                 }];
-        })
+        }])
         .directive('showtab',
                 function () {
                         return {
