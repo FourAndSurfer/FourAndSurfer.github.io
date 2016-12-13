91c8a316f7dce342890e35ee00f55f0917a445f5
Munir Wanis <fallen.angel.pp@gmail.com>
2016-12-13 16:55:38 -0200

    :art: Adds item sub and add on cart, plus value sum

--- www/js/cartController.js
+++ www/js/cartController.js
@@ -4,4 +4,22 @@ angular.module('PizzariaApp')
     self.log = function() {console.log(CartFactory.cart);};
     self.cart = CartFactory.cart;
     self.qty = function() {};
+    self.sumQty = function(cartItem) {
+        cartItem.qty++;
+    };
+    self.subtractQty = function(cartItem) {
+        cartItem.qty--;
+    };
+    self.totalValue = 0;
+    self.totalQty = 0;
+    self.totalValueAndQty = function() {
+        var value = 0;
+        var qty = 0;
+        for (var i = 0; i < self.cart.length; i++) {
+            value += self.cart[i].preco * self.cart[i].qty;
+            qty += self.cart[i].qty;
+        }
+        self.totalValue = value;
+        self.totalQty = qty;
+    };
 }]);
