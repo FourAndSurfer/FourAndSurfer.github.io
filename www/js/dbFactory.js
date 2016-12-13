aae59939f8f88ce38cce457a9ca76e8183cee16c
Munir Wanis <fallen.angel.pp@gmail.com>
2016-12-13 10:55:58 -0200

    :art: Adiciona bebidas e sobremesas no cache

--- www/js/dbFactory.js
+++ www/js/dbFactory.js
@@ -4,6 +4,8 @@ angular.module("PizzariaApp")
 
         response.loadDb = function () {
             response.pizzas = [];
+            response.bebidas = [];
+            response.sobremesas = [];
             var db = window.openDatabase(
                 'pizzaria', // short name
                 '1.0', // version
@@ -11,6 +13,7 @@ angular.module("PizzariaApp")
                 2 * 1024 * 1024 // size in bytes
             );
             db.transaction(function (tx) {
+                // Insere pizzas na tabela
                 // Só descomenta se precisar apagar a tabela!
                 // tx.executeSql('DROP TABLE IF EXISTS pizzas');
                 tx.executeSql('CREATE TABLE IF NOT EXISTS pizzas (id unique, nome, ingredientes, img, preco)');
@@ -21,6 +24,37 @@ angular.module("PizzariaApp")
                         i;
                     for (i = 0; i < len; i++) {
                         response.pizzas.push(results.rows.item(i));
+                    }
+                });
+
+                // Insere bebidas na tabela
+                // Só descomenta se precisar apagar a tabela!
+                // tx.executeSql('DROP TABLE IF EXISTS bebidas');
+                tx.executeSql('CREATE TABLE IF NOT EXISTS bebidas (id unique, nome, descricao, img, preco)');
+                tx.executeSql('INSERT INTO bebidas (id, nome, descricao, img, preco) SELECT 1, "Coca-cola", "Refrigerante 2 litros", "img/bebidas/coca-cola.png", 5.90 WHERE NOT EXISTS (SELECT 1 FROM bebidas WHERE id = 1 AND nome = "Coca-cola")');
+                tx.executeSql('INSERT INTO bebidas (id, nome, descricao, img, preco) SELECT 2, "Coca-Zero", "Refrigerante 2 litros", "img/bebidas/coca-zero.png", 5.90 WHERE NOT EXISTS (SELECT 1 FROM bebidas WHERE id = 2 AND nome = "Coca-Zero")');
+                tx.executeSql('INSERT INTO bebidas (id, nome, descricao, img, preco) SELECT 3, "Guaraná", "Refrigerante 2 litros", "img/bebidas/guarana.png", 5.90 WHERE NOT EXISTS (SELECT 1 FROM bebidas WHERE id = 3 AND nome = "Guaraná")');
+                tx.executeSql('INSERT INTO bebidas (id, nome, descricao, img, preco) SELECT 4, "Guaraná Zero", "Refrigerante 2 litros", "img/bebidas/guarana-zero.png", 5.90 WHERE NOT EXISTS (SELECT 1 FROM bebidas WHERE id = 4 AND nome = "Guaraná Zero")');
+                tx.executeSql('INSERT INTO bebidas (id, nome, descricao, img, preco) SELECT 5, "Sprite", "Refrigerante 2 litros", "img/bebidas/sprite.png", 5.90 WHERE NOT EXISTS (SELECT 1 FROM bebidas WHERE id = 5 AND nome = "Sprite")');
+                tx.executeSql('SELECT nome, descricao, img, preco FROM bebidas', [], function (tx, results) {
+                    var len = results.rows.length,
+                        i;
+                    for (i = 0; i < len; i++) {
+                        response.bebidas.push(results.rows.item(i));
+                    }
+                });
+
+                // Insere sobremesas na tabela
+                // Só descomenta se precisar apagar a tabela!
+                // tx.executeSql('DROP TABLE IF EXISTS sobremesas');
+                tx.executeSql('CREATE TABLE IF NOT EXISTS sobremesas (id unique, nome, descricao, img, preco)');
+                tx.executeSql('INSERT INTO sobremesas (id, nome, descricao, img, preco) SELECT 1, "Dipp\'s de Canela", "A nossa deliciosa massa coberta de açúcar e canela, acompanhada de doce de leite.", "img/sobremesas/dipps.png", 8.99 WHERE NOT EXISTS (SELECT 1 FROM sobremesas WHERE id = 1 AND nome = "Dipp\'s de Canela")');
+                tx.executeSql('INSERT INTO sobremesas (id, nome, descricao, img, preco) SELECT 2, "Chocobread", "Massa finíssima recheada de muito chocolate e cobertura de creme de baunilha com granulado.", "img/sobremesas/chocobread.png", 7.95 WHERE NOT EXISTS (SELECT 1 FROM sobremesas WHERE id = 2 AND nome = "Chocobread")');
+                tx.executeSql('SELECT nome, descricao, img, preco FROM sobremesas', [], function (tx, results) {
+                    var len = results.rows.length,
+                        i;
+                    for (i = 0; i < len; i++) {
+                        response.sobremesas.push(results.rows.item(i));
                     }
                 });
             });
--- www/js/homeController.js
+++ www/js/homeController.js
@@ -7,37 +7,8 @@ angular.module('PizzariaApp')
                 };
 
                 self.pizzaList = DbFactory.pizzas;
-
-                self.bebidaList = [{
-                        nome: "Coca-cola",
-                        descricao: "Refrigerante 2 litros",
-                        img: "img/coca-cola.png"
-                }, {
-                        nome: "Coca-Zero",
-                        descricao: "Refrigerante 2 litros",
-                        img: "img/coca-zero.png"
-                }, {
-                        nome: "Guaraná",
-                        descricao: "Refrigerante 2 litros",
-                        img: "img/guarana.png"
-                }, {
-                        nome: "Guaraná Zero",
-                        descricao: "Refrigerante 2 litros",
-                        img: "img/guarana-zero.png"
-                }, {
-                        nome: "Sprite",
-                        descricao: "Refrigerante 2 litros",
-                        img: "img/sprite.png"
-                }];
-                self.sobremesaList = [{
-                        nome: "DIPP'S DE CANELA",
-                        descricao: "A deliciosa massa da Domino's coberta de açúcar e canela, acompanhada de doce de leite.",
-                        img: "img/dipps.png"
-                }, {
-                        nome: "CHOCOBREAD",
-                        descricao: "Massa finíssima recheada de muito chocolate e cobertura de creme de baunilha com granulado.",
-                        img: "img/chocobread.png"
-                }];
+                self.bebidaList = DbFactory.bebidas;
+                self.sobremesaList = DbFactory.sobremesas;
         }])
         .directive('showtab',
                 function () {
