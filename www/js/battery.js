c2be519fe9b80152e3f9542caf228521c6f3fd80
Rodrigo Filomeno <Rodrigo Filomeno>
2016-12-21 17:43:46 -0200

    tentando colocar plugin da bateria... sem sucesso =(

--- www/js/battery.js
+++ www/js/battery.js
@@ -1,10 +1,11 @@
 window.addEventListener("batterylow", onBatteryLow, false);
 
-        function onBatteryLow(status) {
-            alert("Battery Level Low " + status.level + "%");
+function onBatteryLow(status) {
+    alert("Bateria fraca: " + status.level + "%");
 };
-        window.addEventListener("batterycritical", onBatteryCritical, false);
 
-        function onBatteryCritical(status) {
-            alert("Battery Level Critical " + status.level + "%\nRecharge Soon!");
-}
+window.addEventListener("batterycritical", onBatteryCritical, false);
+
+function onBatteryCritical(status) {
+    alert("Bateria Critica! Por favor conectar uma fonte de energia. " + status.level + "%");
+};
