f0c568ac01bed42e82c747a501bed7cfb356e0cf
Rodrigo Filomeno <Rodrigo Filomeno>
2016-12-22 14:27:24 -0200

    bla

--- www/js/usuarioLogado.js
+++ www/js/usuarioLogado.js
@@ -1,13 +1,13 @@
 window.usuarioLogado = {
-            nome,
-            email,
-            tel1,
-            tel2,
-            rua,
-            compl,
-            bairro,
-            cep,
-            password,
-            fbId
+    nome: '',
+    email: '',
+    tel1: '',
+    tel2: '',
+    rua: '',
+    compl: '',
+    bairro: '',
+    cep: '',
+    password: '',
+    fbId: ''
 };
 window.logado = 0;
